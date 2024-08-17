e9a75009c783dadcabb383515ba7381f94fbd58c
alexmejia0112 <alexmejia0112@gmail.com>
2024-08-17 16:18:08 +0000

    Ejercicios clase

--- clase2/ejercicio4.py
+++ clase2/ejercicio4.py
@@ -5,7 +5,7 @@ peso = int(input("Ingrese su peso en kilogramos: "))
 altura = float(input("Ingrese su altura en metros: "))
 
 # Calculo indice de masa corporal 
-imc = peso / (altura * altura)
+imc = peso / (altura ** 2)
 
 if imc < 18.5:
     print("Se encuentra en bajo peso")
