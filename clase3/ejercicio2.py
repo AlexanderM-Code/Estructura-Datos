67edb741985064090cdc2dcd2d09c2701ee198f2
AlexanderM-Code <alexmejia0112@gmail.com>
2024-08-24 17:47:21 +0000

    Ejercicios de practica clase 3

---
+++ clase3/ejercicio2.py
@@ -0,0 +1,2 @@
+pares = [x for x in range(2,101,2)]
+print(pares)
---
+++ clase3/ejercicio3.py
@@ -0,0 +1,6 @@
+while True:
+    print("-"*50)
+    altura = int(input("Ingrese la altura del triangulo: "))
+    print("-"*50)
+    for x in range(1, altura+1):
+        print('*'*x)
