728608de79ccd06082a38935f3da0bf8888d318d
AlexanderM-Code <alexmejia0112@gmail.com>
2024-08-31 13:54:43 +0000

    Ejercicios clase 3

--- clase3/ejercicio4.py
+++ clase3/ejercicio4.py
@@ -1,1 +1,8 @@
-numeros_aleatorios = 
+import random
+
+numeros = [random.randint(1, 100) for _ in range(10)]
+
+promedio = sum(numeros) / len(numeros)
+
+print("Números:", numeros)
+print("Promedio:", promedio)
---
+++ ejercicio5.py
@@ -0,0 +1,9 @@
+numero = 5
+
+factorial = 1
+i = 1
+while i <= numero:
+    factorial *= i
+    i += 1
+
+print("El factorial de", numero, "es:", factorial)
